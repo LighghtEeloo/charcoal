56b13d710d96d7875aa44334eed08dc1cf51eff9
LighghtEeloo <litiaeeloo@gmail.com>
2022-06-17 08:20:17 -0400

    Fix. Sentence punctuators.

--- src/select/sentence.rs
+++ src/select/sentence.rs
@@ -7,13 +7,26 @@ impl Select for Sentence {
 
     fn select(elem: ElementRef) -> anyhow::Result<Self::Target> {
         let sel = Selector::parse("#bilingual.trans-container li").unwrap();
-        Ok(elem.select(&sel).filter_map(|child| {
-            Sen::select(child).ok()
-        }).collect())
+        Ok(elem
+            .select(&sel)
+            .filter_map(|child| Sen::select(child).ok())
+            .collect())
     }
 }
 
 struct Sen;
+const PUNCTUATORS: &[char; 10] = &[
+    '.',
+    ',',
+    '\"',
+    '\'',
+    '?',
+    '!',
+    ':',
+    '-',
+    '<',
+    '>',
+];
 
 impl Select for Sen {
     type Target = (String, String);
@@ -22,15 +35,23 @@ impl Select for Sen {
         let sel = Selector::parse("p").unwrap();
         let mut iter = elem.select(&sel);
 
-        fn text_and_join(elem: ElementRef, sep: &str) -> String {
-            elem.text()
-                .filter_map(trim_str)
-                .collect::<Vec<String>>()
-                .join(sep)
+        fn text_to_vec(elem: ElementRef) -> Vec<String> {
+            elem.text().filter_map(trim_str).collect()
         }
 
-        let ori = text_and_join(iter.next().expect("No ori found in sentence"), " ");
-        let trans = text_and_join(iter.next().expect("No trans found in sentence"), "");
+        let mut ori = String::new();
+        let mut ori_iter = text_to_vec(iter.next().expect("No ori found in sentence")).into_iter();
+        if let Some(s) = ori_iter.next() {
+            ori.push_str(&s)
+        }
+        for mut s in ori_iter {
+            if !s.starts_with(PUNCTUATORS) {
+                s.insert(0, ' ')
+            } 
+            ori.push_str(&s)
+        }
+
+        let trans = text_to_vec(iter.next().expect("No trans found in sentence")).join("");
         Ok((ori, trans))
     }
 }
