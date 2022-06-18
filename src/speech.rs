c20edcbf4ace5c38fb0d5c8b3da7f193f8a74284
LighghtEeloo <litiaeeloo@gmail.com>
2022-06-18 06:51:07 -0400

    Incr. Speech.

--- src/speech.rs
+++ src/speech.rs
@@ -41,35 +41,41 @@ pub struct Speech;
 impl Speech {
     pub async fn query(word: impl AsRef<str>, cache: &Cache, is_speak: bool) -> anyhow::Result<()> {
         if is_speak {
-            Speech::speak(word, cache).await
+            let file = Speech::store(word, cache).await?;
+            Speech::speak(file).await
         } else {
             Ok(())
         }
     }
 
-    async fn speak(word: impl AsRef<str>, cache: &Cache) -> anyhow::Result<()> {
-        let url = format!(
+    fn url(word: impl AsRef<str>) -> String {
+        format!(
             "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl={}&q={}",
             Lang::new(&word),
             word.as_ref()
-        );
+        )
+    }
 
+    async fn store(word: impl AsRef<str>, cache: &Cache) -> anyhow::Result<File> {
         let file = (cache.query(&word, "mp3")).or_else(|_| -> anyhow::Result<File> {
             futures::executor::block_on(async {
                 // request
-                let res = reqwest::get(url).await?;
+                let res = reqwest::get(Speech::url(&word)).await?;
 
                 // write
                 let mut file = cache.store(&word, "mp3")?;
                 let bytes = res.bytes().await?;
                 file.write_all(&bytes)?;
 
-                // read again
+                // read again to avoid overflow
                 let file = cache.query(&word, "mp3")?;
                 Ok(file)
             })
         })?;
+        Ok(file)
+    }
 
+    async fn speak(file: File) -> anyhow::Result<()> {
         // rodio
         // Get a output stream handle to the default physical sound device
         let (_stream, stream_handle) = OutputStream::try_default()?;
