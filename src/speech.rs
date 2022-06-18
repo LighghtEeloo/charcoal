48b05a4e521163674d969ed17578ca338e576e6d
LighghtEeloo <litiaeeloo@gmail.com>
2022-06-18 03:07:58 -0400

    Impl. Rodio for playing audio.

--- src/speech.rs
+++ src/speech.rs
@@ -1,15 +1,68 @@
 use crate::Config;
+use rodio::{Decoder, OutputStream, Sink};
+use std::{
+    fs::File,
+    io::{BufReader, Write},
+    path::PathBuf,
+};
+
+/// Currently only a minimium set of langs are supported.
+/// Considering adding more in the future.
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone)]
+pub enum Lang {
+    en_us,
+    zh_cn,
+}
+
+impl Lang {
+    pub fn new(word: impl AsRef<str>) -> Self {
+        // worst way to distinguish language,
+        // NOT ELEGANT :-(
+        // but it works.
+        if word.as_ref().is_ascii() {
+            Lang::en_us
+        } else {
+            Lang::zh_cn
+        }
+    }
+}
+
+impl std::fmt::Display for Lang {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Lang::en_us => write!(f, "en_us"),
+            Lang::zh_cn => write!(f, "zh_cn"),
+        }
+    }
+}
 
 async fn __speak(word: impl AsRef<str>) -> anyhow::Result<()> {
-    // let mut ttss = tts::Tts::default()?;
-    // let tts::Features { is_speaking, .. } = ttss.supported_features();
-    // if is_speaking {
-    //     println!("Are we speaking? {}", ttss.is_speaking()?);
-    // }
-    // ttss.speak(word.word, false)?;
+    let url = format!(
+        "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl={}&q={}",
+        Lang::new(&word),
+        word.as_ref()
+    );
+    let filename = PathBuf::from("./audio.mp3");
 
-    let voice = google_speech::Speech::new(word, google_speech::Lang::en_us)?;
-    voice.play()?;
+    // request
+    let res = reqwest::get(url).await?;
+
+    let mut file = File::create(&filename)?;
+    let bytes = res.bytes().await?;
+    file.write_all(&bytes)?;
+
+    // rodio
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+
+    let file = BufReader::new(File::open(&filename)?);
+    let source = Decoder::new(file)?;
+    sink.append(source);
+    sink.sleep_until_end();
+
+    std::fs::remove_file(filename)?;
     Ok(())
 }
 
