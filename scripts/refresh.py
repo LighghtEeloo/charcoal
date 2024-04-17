1f3ecc4ee6719baedbc6e0685de4bb460aa65172
LighghtEeloo <litiaeeloo@gmail.com>
2024-04-17 06:25:45 -0400

    incr: refresh script

--- scripts/refresh.py
+++ scripts/refresh.py
@@ -1,6 +1,15 @@
 import os
 
 if __name__ == "__main__":
-    with open("~/.zhistory", 'r') as f:
+    home = os.path.expanduser("~")
+    with open(os.path.join(home, ".zhistory"), 'r') as f:
         lines = [line for line in f.readlines() if line.startswith("ww ")]
-    print(lines)
+    lines = [line.rstrip().lstrip("ww ") for line in lines]
+    # for line in lines:
+    #     for letter in line:
+    #         # report if letter is not english
+    #         if not letter.isalpha() and not letter.isspace() and not letter == '-':
+    #             print(line)
+    #             break
+    for line in lines:
+        os.system(f"charcoal query {line} -s --refresh")
